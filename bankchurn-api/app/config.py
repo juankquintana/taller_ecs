5aff8ae6a258cf70b5e671159de633db28247b4d
Juan Camilo Quintana Uniandes <jc.quintana@uniandes.edu.co>
2024-11-13 23:02:50 -0500

    cambio nombre proyecto

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "JUAN CAMILO QUINTANA"
 
     class Config:
         case_sensitive = True
